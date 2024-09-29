b42de67d84e8a13741848ba1abbd2903ec93e8f1
krabbypaty <hlee60@stuy.edu>
2024-09-29 17:46:22 -0400

    Finished returnCopy

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,7 +1,14 @@
 public class MyArrays {
   public static void main(String[] args) {
-  int[] ary = {5, 4, 3, 2, 1};
-  System.out.println(arrayToString(ary));
+    int[] ary = { 5, 4, 3, 2, 1 };
+    int[] ary2 = { 51, 2, -4, 0, 0 };
+    int[] ary3 = { 3, 5, 1, -1, 23 };
+    int[] ary4 = { 5, 4, 3, 2, 1 };
+    int[] ary5 = { 5, 4, 3, 2, 1 };
+    //arrayToString tests
+    System.out.println(arrayToString(ary));
+    System.out.println(arrayToString(ary2));
+    System.out.println(arrayToString(returnCopy(ary)));
   }
 
   public static String arrayToString(int[] ary) {
@@ -12,6 +19,14 @@ public class MyArrays {
         newAry += ", ";
       }
     }
-  return newAry + "]";
-}
+    return newAry + "]";
+  }
+
+  public static int[] returnCopy(int[] ary) {
+    int[] newAry = new int[ary.length];
+    for (int i = 0; i < ary.length; i++) {
+      newAry[i] = ary[i];
+    }
+    return newAry;
+  }
 }
