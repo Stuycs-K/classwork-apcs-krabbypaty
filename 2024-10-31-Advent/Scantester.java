508776940f4f986b1e2e5fd11a0616548394e8e3
Harrison Lee <harrisonl3570@gmail.com>
2024-10-31 10:02:46 -0400

    tested print by word and sum

---
+++ 2024-10-31-Advent/Scantester.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Scantester {
+
+public static void main(String args[]) {
+  Scanner sc1 = new Scanner("This is a bunch of words");
+  Scanner sc2 = new Scanner("10.0 1.04 99.0 -3.0 4.2");
+  while (sc1.hasNext()) {
+    System.out.println(sc1.next());
+  }
+  System.out.println(sum(sc2));
+}
+public static int sum(Scanner x) {
+int sum = 0;
+while(x.hasNextDouble()){
+  sum += x.nextDouble();
+}
+return sum;
+}
+}
