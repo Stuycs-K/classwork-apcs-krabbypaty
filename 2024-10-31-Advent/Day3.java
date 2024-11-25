f934b6cf9d233b908ec2b8437fde363a844a19ad
krabbypaty <hlee60@stuy.edu>
2024-11-25 01:20:52 -0500

    finished

--- 2024-10-31-Advent/Day3.java
+++ 2024-10-31-Advent/Day3.java
@@ -22,16 +22,25 @@ public class Day3 {
       }
 
       public static String message(String[] input) {
-        int[] values = new int[26];
+        String output = "";
         for (int i = 0; i < input[0].length(); i++) {
+          int[] values = new int[26];
             for (int idx = 0; idx < input.length; idx++) {
                 char character = input[idx].charAt(i);
                 int ascii = character - 97;
                 values[ascii]++;
             }
+            int highest = 0;
+            int index = 0;
+            for (int idx = 0; idx < values.length; idx++) {
+              if (values[idx] > highest) {
+                highest = values[idx];
+                index = idx;
+              }
+            }
+            output += Character.toString(index + 97);
         }
-        System.out.println(Arrays.toString(values));
-        return "pog";
+        System.out.println(output);
       }
 
     public static void main(String[] args) {
