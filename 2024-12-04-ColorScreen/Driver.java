1cfee634a9f8e21a27dcb3fd07c85de0a714aead
krabbypaty <hlee60@stuy.edu>
2024-12-04 22:35:16 -0500

    Put the random integers in place

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -7,14 +7,16 @@ public class Driver {
         String backChar = "#"; 
         
         Text.color(color2);
-        for (int i = 0; i < 80; i++) {
+        Text.go(1, 1);
+        for (int i = 1; i < 81; i++) {
             System.out.print(backChar);
         }
-        System.out.println();
-        for (int i =0; i < 30; i++) {
+        for (int i =1; i < 31; i++) {
+            Text.go(i + 1, 1);
             System.out.println(backChar + " ".repeat(78) + backChar);
         }
-        for (int i = 0; i < 80; i++) {
+        Text.go(31, 1);
+        for (int i = 1; i < 81; i++) {
             System.out.print(backChar);
         }
     }
@@ -24,11 +26,27 @@ public class Driver {
         for (int i = 0; i < randomInt.length; i++) {
             randomInt[i] = (int) (Math.random() * 99);
         }
-        System.out.println(Arrays.toString(randomInt));
+        Text.go(2, 2);
+        for (int i = 0; i < randomInt.length; i++) {
+            if (randomInt[i] < 25) {
+                Text.color(Text.RED);
+            }
+            else if (randomInt[i] >75) {
+                Text.color(Text.GREEN);
+            }
+            else {
+                Text.color(Text.WHITE);
+            }
+            System.out.print(" ".repeat(20) + randomInt[i]);
+        }
+        //System.out.print(" ".repeat(20) + randomInt[0] + " ".repeat(20) + randomInt[1] + " ".repeat(20) + randomInt[2]);
     }
     public static void main(String[] args) {
+        System.out.print(Text.CLEAR_SCREEN);
+        System.out.print(Text.HIDE_CURSOR);
         printBackground(Text.CYAN);
         randomInteger();
+        System.out.println(Text.RESET);
     }
 }
 
