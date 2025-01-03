cae6b1dcfeab098ed00bd276404850da8de98890
Harrison Lee <harrisonl3570@gmail.com>
2025-01-03 10:15:23 -0500

    finished makewords

--- 2025-01-02/makeWords.java
+++ 2025-01-02/makeWords.java
@@ -1,10 +1,17 @@
 public class makeWords {
   public static void makeWords(int remainingLetters, String result, String alphabet){
+    int counter = 0;
     if (remainingLetters == 0) {
       System.out.println(result);
     }
     else {
-      makeWords(remainingLetters - 1, result, alphabet)
+      for (int i = 0; i < alphabet.length(); i++) {
+      makeWords(remainingLetters - 1, result + alphabet.substring(i, i+1), alphabet);
     }
+    }
+}
+
+public static void main(String[] args) {
+  makeWords(8, "", "abcde");
 }
 }
